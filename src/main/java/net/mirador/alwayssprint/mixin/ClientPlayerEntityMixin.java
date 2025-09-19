4b8da68b8c5470b564e86528fa1ff39b57f05996
Joschas Thinkpad <goetzjoscha@gmail.com>
2025-09-19 10:19:14 +0200

    Test build.yml

--- src/main/java/net/mirador/alwayssprint/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/net/mirador/alwayssprint/mixin/ClientPlayerEntityMixin.java
@@ -2,7 +2,6 @@ package net.mirador.alwayssprint.mixin;
 
 import com.mojang.authlib.GameProfile;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.input.Input;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
@@ -12,7 +11,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ClientPlayerEntity.class)
 public abstract class ClientPlayerEntityMixin extends AbstractClientPlayerEntity {
@@ -24,13 +22,6 @@ public abstract class ClientPlayerEntityMixin extends AbstractClientPlayerEntity
     @Final
     protected MinecraftClient client;
 
-    /**
-     * Changed injection point to HEAD to allow vanilla checks to take place
-     * after we set sprinting to true.
-     * <p>
-     * This should allow the user to always sprint as long as the vanilla checks
-     * are passed.
-     */
     @Inject(method = "tickMovement", at = @At("HEAD"))
     public void tickMovement(CallbackInfo ci) {
         this.client.options.sprintKey.setPressed(true);
