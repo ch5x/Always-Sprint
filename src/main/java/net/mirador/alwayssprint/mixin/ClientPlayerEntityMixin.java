116df838fd4e724f78aebf8e8632b9eda3e513d1
chax <117924292+ch5x@users.noreply.github.com>
2026-03-24 20:20:42 -0300

    26.1

--- src/main/java/net/mirador/alwayssprint/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/net/mirador/alwayssprint/mixin/ClientPlayerEntityMixin.java
@@ -1,10 +1,10 @@
 package net.mirador.alwayssprint.mixin;
 
 import com.mojang.authlib.GameProfile;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.client.world.ClientWorld;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.multiplayer.ClientLevel;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -12,18 +12,18 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientPlayerEntity.class)
-public abstract class ClientPlayerEntityMixin extends AbstractClientPlayerEntity {
-    public ClientPlayerEntityMixin(ClientWorld world, GameProfile profile) {
+@Mixin(LocalPlayer.class)
+public abstract class ClientPlayerEntityMixin extends AbstractClientPlayer {
+    public ClientPlayerEntityMixin(ClientLevel world, GameProfile profile) {
         super(world, profile);
     }
 
     @Shadow
     @Final
-    protected MinecraftClient client;
+    protected Minecraft minecraft;
 
-    @Inject(method = "tickMovement", at = @At("HEAD"))
-    public void tickMovement(CallbackInfo ci) {
-        this.client.options.sprintKey.setPressed(true);
+    @Inject(method = "aiStep", at = @At("HEAD"))
+    public void aiStep(CallbackInfo ci) {
+        this.minecraft.options.keySprint.setDown(true);
     }
 }
